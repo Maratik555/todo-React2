482902f531f713956bc052a3a5699842f6d1b7f2
Маратик555 <amr404@bk.ru>
2022-02-11 00:58:07 +0300

    Updates

---
+++ precache-manifest.49816a5ca50761a9b423436b1c8cc666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba12db73e1a6cc322f52f85a8992cd2",
+    "url": "/todo-React2/index.html"
+  },
+  {
+    "revision": "4c8140743418dbadffd0",
+    "url": "/todo-React2/static/css/main.f397faf5.chunk.css"
+  },
+  {
+    "revision": "899712c8debbce5cb876",
+    "url": "/todo-React2/static/js/2.a277081d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-React2/static/js/2.a277081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8140743418dbadffd0",
+    "url": "/todo-React2/static/js/main.ef9b11e5.chunk.js"
+  },
+  {
+    "revision": "2f73a1058610096037e8",
+    "url": "/todo-React2/static/js/runtime-main.4583c449.js"
+  }
+]);
